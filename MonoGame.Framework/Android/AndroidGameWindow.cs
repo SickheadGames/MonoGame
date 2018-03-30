1004f7d05921fd80536d3cfb55a6fbc0f4364621
Tom Spilman <tom@sickheadgames.com>
2018-03-30 09:11:53 -0500

    Fake mouse from touches on Android.

--- MonoGame.Framework/Android/AndroidGameWindow.cs
+++ MonoGame.Framework/Android/AndroidGameWindow.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Views;
 using Microsoft.Xna.Framework.Input.Touch;
 using MonoGame.OpenGL;
+using Microsoft.Xna.Framework.Input;
 
 namespace Microsoft.Xna.Framework
 {
@@ -86,6 +87,21 @@ namespace Microsoft.Xna.Framework
             {
                 if (!GameView.IsResuming && _game.Platform.IsActive && !ScreenReceiver.ScreenLocked) //Only call draw if an update has occured
                 {
+                    // Stardew hack:  Report first touch point mouse state.
+                    {
+                        var mstate = TouchPanel.PrimaryWindow.MouseState;
+                        var tstate = TouchPanel.GetState();
+                        if (tstate.Count == 0)
+                            mstate.LeftButton = ButtonState.Released;
+                        else
+                        {
+                            mstate.X = (int)tstate[0].Position.X;
+                            mstate.Y = (int)tstate[0].Position.Y;
+                            mstate.LeftButton = ButtonState.Pressed;
+                        }
+                        TouchPanel.PrimaryWindow.MouseState = mstate;
+                    }
+
                     _game.Tick();
                 }
                 else if (_game.GraphicsDevice != null)
