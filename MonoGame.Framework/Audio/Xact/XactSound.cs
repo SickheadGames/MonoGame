85795f545e41b2aa8c27e1c696de560f123b42b3
James Ford <james@sickheadgames.com>
2018-06-14 18:16:44 -0500

    fix to streaming sound effect instances getting stuck forever in the playing list

--- MonoGame.Framework/Audio/Xact/XactSound.cs
+++ MonoGame.Framework/Audio/Xact/XactSound.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Xna.Framework.Audio
         private float? _cueFilterQFactor;
 
         internal readonly int[] RpcCurves;
-        
+
         public XactSound(SoundBank soundBank, int waveBankIndex, int trackIndex)
         {
             _complexSound = false;
@@ -43,7 +43,7 @@ namespace Microsoft.Xna.Framework.Audio
         public XactSound(AudioEngine engine, SoundBank soundBank, BinaryReader soundReader)
         {
             _soundBank = soundBank;
-            
+
             var flags = soundReader.ReadByte();
             _complexSound = (flags & 0x1) != 0;
             var hasRPCs = (flags & 0x0E) != 0;
@@ -54,11 +54,11 @@ namespace Microsoft.Xna.Framework.Audio
             _pitch = soundReader.ReadInt16() / 1000.0f;
             soundReader.ReadByte(); //priority
             soundReader.ReadUInt16(); // filter stuff?
-            
+
             var numClips = 0;
             if (_complexSound)
                 numClips = soundReader.ReadByte();
-            else 
+            else
             {
                 _trackIndex = soundReader.ReadUInt16();
                 _waveBankIndex = soundReader.ReadByte();
@@ -87,7 +87,7 @@ namespace Microsoft.Xna.Framework.Audio
                 _useReverb = false;
             else
             {
-                // The file format for this seems to follow the pattern for 
+                // The file format for this seems to follow the pattern for
                 // the RPC curves above, but in this case XACT only supports
                 // a single effect...  Microsoft Reverb... so just set it.
                 _useReverb = true;
@@ -144,22 +144,20 @@ namespace Microsoft.Xna.Framework.Audio
             float finalPitch = _pitch + _cuePitch;
             float finalMix = _useReverb ? _cueReverbMix : 0.0f;
 
-            if (_complexSound) 
+            if (_complexSound)
             {
                 foreach (XactClip clip in _soundClips)
                 {
                     clip.UpdateState(finalVolume, finalPitch, finalMix, _cueFilterFrequency, _cueFilterQFactor);
                     clip.Play();
                 }
-            } 
-            else 
+            }
+            else
             {
                 if (_wave != null)
                 {
-                    if (_streaming)
-                        _wave.Dispose();
-					else
-						_wave._isXAct = false;					
+                    _wave.Dispose();
+                    _wave._isXAct = false;
                     _wave = null;
                 }
 
@@ -190,10 +188,8 @@ namespace Microsoft.Xna.Framework.Audio
             {
                 if (_wave != null && _wave.State == SoundState.Stopped)
                 {
-                    if (_streaming)
-                        _wave.Dispose();
-					else
-						_wave._isXAct = false;					
+                    _wave.Dispose();
+                    _wave._isXAct = false;
                     _wave = null;
                 }
             }
@@ -210,16 +206,15 @@ namespace Microsoft.Xna.Framework.Audio
             {
                 if (_wave != null)
                 {
-                    _wave.Stop();
-                    if (_streaming)
-                        _wave.Dispose();
- 					else
-						_wave._isXAct = false;					
-                   _wave = null;
+                    if (_wave.State != SoundState.Stopped)
+                        _wave.Stop();
+                    _wave.Dispose();
+                    _wave._isXAct = false;
+                    _wave = null;
                 }
             }
         }
-        
+
         public void Stop(AudioStopOptions options)
         {
             if (_complexSound)
@@ -231,16 +226,15 @@ namespace Microsoft.Xna.Framework.Audio
             {
                 if (_wave != null)
                 {
-                    _wave.Stop();
-                    if (_streaming)
-                        _wave.Dispose();
-					else
-						_wave._isXAct = false;					
+                    if (_wave.State != SoundState.Stopped)
+                        _wave.Stop();
+                    _wave.Dispose();
+                    _wave._isXAct = false;
                     _wave = null;
                 }
             }
         }
-        
+
         public void Pause()
         {
             if (_complexSound)
@@ -257,7 +251,7 @@ namespace Microsoft.Xna.Framework.Audio
                     _wave.Pause();
             }
         }
-                
+
         public void Resume()
         {
             if (_complexSound)
@@ -331,9 +325,9 @@ namespace Microsoft.Xna.Framework.Audio
             }
         }
 
-        public bool Playing 
+        public bool Playing
         {
-            get 
+            get
             {
                 if (_complexSound)
                 {
@@ -342,7 +336,7 @@ namespace Microsoft.Xna.Framework.Audio
                             return true;
 
                     return false;
-                } 
+                }
 
                 return _wave != null && _wave.State == SoundState.Playing;
             }
@@ -375,10 +369,10 @@ namespace Microsoft.Xna.Framework.Audio
         {
             get
             {
-                if (_complexSound) 
+                if (_complexSound)
                 {
                     foreach (var clip in _soundClips)
-                        if (clip.State == SoundState.Paused) 
+                        if (clip.State == SoundState.Paused)
                             return true;
 
                     return false;
